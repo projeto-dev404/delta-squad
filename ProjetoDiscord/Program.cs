31c362c9528606a4ea6d9c1e4aa23ef3f8997e82
Julio Cezar <juliocezarcontatodev@gmail.com>
2023-09-27 11:48:16 -0300

    Configuracao para que o projeto inicial funcione corretamente

--- ProjetoDiscord/Program.cs
+++ ProjetoDiscord/Program.cs
@@ -14,14 +14,6 @@ namespace ProjectDelta
         {
             Inicializacao ini = new Inicializacao();
 
-            Usuario usuario = new Usuario();
-            usuario.Nome = "Teste";
-            usuario.Login = "teste";
-            usuario.Senha = "123456";
-
-            var user = new Usuario();
-            user.ExcluirUsuario("a");
-            var v = user.VerificarSenha(usuario.Login, usuario.Senha);
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
